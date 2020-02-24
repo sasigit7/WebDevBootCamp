9a47b5f445174d0488fb92b48e718a96675b1c4d
shashidar mandalozu <sasitech18@gmail.com>
2020-02-24 21:20:10 +0000

    Change colors and pick colors functionality

--- A.ColorGameProject/colorGame.js
+++ A.ColorGameProject/colorGame.js
@@ -6,6 +6,7 @@ var colors = [
     "rgb(0, 0, 255)",
     "rgb(255, 0, 255)"
 ];
+var messageDisplay = document.getElementById("message");
 
 // Select squares and apply different colors from colors array above.
 var squares = document.querySelectorAll(".square");
@@ -18,14 +19,32 @@ for (var i = 0; i < squares.length; i++) {
         var clickedColor = this.style.backgroundColor;
         // Compare color to picked color
         if (clickedColor === pickedColor) {
-            alert("Correct!");
+            // alert("Correct!");
+            messageDisplay.textContent = "Correct";
+            changeColors(clickedColor);
         } else {
-            alert("Wrong!!");
+            // alert("Wrong!!");
+            this.style.backgroundColor = "#232323";
+            messageDisplay.textContent = "Try again";
         }
     });
 }
 
-// Style
-var pickedColor = colors[3];
+function changeColors(color) {
+    //Loop through the squares
+    for (var i = 0; i < squares.length; i++) {
+        // Change each color to match given color
+        squares[i].style.backgroundColor = color;
+    }
+}
+
+// Style the header 
+// var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 colorDisplay.textContent = pickedColor;
+
+function pickColor() {
+    var random = Math.floor(Math.random() * colors.length);
+    return colors[random];
+}
