a5132107d358870e0eaf723c60ec57a7bb9fc25f
shashidar mandalozu <sasitech18@gmail.com>
2020-02-25 19:13:37 +0000

    Added easy and hard buttons functionality

--- A.ColorGameProject/colorGame.js
+++ A.ColorGameProject/colorGame.js
@@ -1,15 +1,50 @@
-var colors = generateRandomColors(6);
+var numSquares = 6; 
+var colors = generateRandomColors(numSquares);
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
 var resetButton = document.querySelector("#reset");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
+
+
+easyBtn.addEventListener("click", function() {
+   //alert("EASY BUTTON CLICKED");
+   hardBtn.classList.remove("selected");
+   easyBtn.classList.add("selected");
+   numSquares = 3;
+   colors = generateRandomColors(numSquares);
+   pickedColor = pickColor();
+   colorDisplay.textContent = pickedColor;
+   for(var i = 0; i < squares.length; i++) {
+       if(colors[i]) {
+           squares[i].style.backgroundColor = colors[i];
+       } else {
+           squares[i].style.display = "none";
+       }
+   }
+});
+
+hardBtn.addEventListener("click", function() {
+    //alert("HARD BUTTON CLICKED");
+    hardBtn.classList.add("selected");
+    easyBtn.classList.remove("selected");
+    numSquares = 6;
+    colors = generateRandomColors(numSquares);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    for(var i = 0; i < squares.length; i++) {
+           squares[i].style.backgroundColor = colors[i];
+           squares[i].style.display = "block";
+       }
+ });
 
 resetButton.addEventListener("click", function() {
   // alert("CLICKED RESET BUTTON");
   // Generate all new colors
-  colors = generateRandomColors(6);
+  colors = generateRandomColors(numSquares);
   //Pick a new random color from an array
   pickedColor = pickColor();
   // change colorDisplay to match picked color
