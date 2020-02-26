18a950253040f5fab1179bd6d2699a4d72037907
shashidar mandalozu <sasitech18@gmail.com>
2020-02-26 20:53:08 +0000

    Marking Completion

--- B.TodoListProject/assets/js/todos.js
+++ B.TodoListProject/assets/js/todos.js
@@ -1,2 +1,36 @@
 //alert("connected");
 
+// Check off Specific Todos by Clicking 
+//$("li").click(function() {
+  // alert("Clicked Li");
+  //$("li").css("color", "gray");
+
+  //$(this).css("color", "gray");
+  //$(this).css("text-decoration", "line-through");
+  
+  //  $(this).css({
+  //    color: "gray",
+  //    textDecoration: "line-through"
+  // });
+
+  // Toggle effect using if else condition:
+//  if($(this).css("color") === "rgb(128, 128, 128)") { // If li is gray
+//      // turn it black
+//      $(this).css({
+//         color: "black",
+//         textDecoration: "none"
+//         });
+//     } else {
+//       // turn it gray 
+//      $(this).css({
+//         color: "gray",
+//         textDecoration: "line-through"
+//      });
+//     }
+//  });
+
+ // Short cut using toggle class: 
+ // Create a class in css file and then use here
+ $("li").click(function() {
+   $(this).toggleClass("completed");
+ });
