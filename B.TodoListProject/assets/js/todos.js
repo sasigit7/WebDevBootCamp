5de36ca444d8bf77a4feffbe1ba0637753da163d
shashidar mandalozu <sasitech18@gmail.com>
2020-02-26 22:26:55 +0000

    Deleting todos functionality

--- B.TodoListProject/assets/js/todos.js
+++ B.TodoListProject/assets/js/todos.js
@@ -34,3 +34,15 @@
  $("li").click(function() {
    $(this).toggleClass("completed");
  });
+
+//  Click on X to Delete todo
+$("span").click(function(event){
+  //alert("Clicked on a span!");
+  // $(this).remove(); // Removes just a span "X"
+ // $(this).parent().remove(); // Removes an entire "li" element on clicking "X"
+  $(this).parent().fadeOut(500, function() {
+      $(this).remove(); // Slowly fades out and removes entire "li" element on clicking "X" 
+  }); 
+  event.stopPropagation(); // Stops firing further events 
+});
+
