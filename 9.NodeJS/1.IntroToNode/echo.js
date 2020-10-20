23b1088b66e5896b4ed226f29f737422ee4f81ff
shashidar mandalozu <sasitech18@gmail.com>
2020-10-21 00:12:36 +0100

    Deleted ignored files

--- 9.NodeJS/1.IntroToNode/echo.js
+++
@@ -1,8 +0,0 @@
-function echo(str, num) {
-    for (var i = 1; i <= num; i++) {
-      console.log(str);
-    }
-  }
-
-echo("Echo!!!", 10) // Should print Echo!!! 10 times
-echo("Tater Tots", 3) // Should print Tater Tots 3 times
--- 9.NodeJS/2.IntroToNPM/DemoApp/app.js
+++
@@ -1,9 +0,0 @@
-console.log("Hello NPM From app.js");
-
-// Random cat Pics
-var cat = require("cat-me");
-console.log(cat());
-
-// Knock Knock Jokes 
-var joke = require("knock-knock-jokes");
-console.log(joke());
